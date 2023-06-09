2a4a9f0aa2eac25f315a26dacee744e2bd63024c
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-06-09 03:32:48 +0100

    added banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home({
   <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
     
     <Header />
-    <main>
+    <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
       <Banner netflixOriginals={netflixOriginals} />
       <section></section>
     </main>
@@ -41,7 +41,7 @@ export const getServerSideProps = async () => {
     horrorMovies,
     romanceMovies,
     documentaries,
-  ] = await Promise.all([
+  ] = await Promise?.all([
     fetch(requests.fetchNetflixOriginals).then(response => response.json()),
     fetch(requests.fetchTrending).then(response => response.json()),
     fetch(requests.fetchTopRated).then(response => response.json()),
