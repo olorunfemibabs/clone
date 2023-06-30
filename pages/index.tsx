9a67f323cfd75bc5d44544bc3b03dec42b92ce99
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-07-01 00:06:09 +0100

    added modal state recoil

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,7 @@ export default function Home({
     </Head>
 
     <Header />
+    
     <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
       <Banner netflixOriginals={netflixOriginals} />
       <section className="md:space-y-24">
